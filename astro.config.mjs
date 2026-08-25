36a07ba935f295227126053aa10dcac75ff33721
permutalibrecol <permutalibre@gmail.com>
2026-08-25 03:57:50 -0500

    Update base path and add Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://permutalibrecol.github.io',
-  base: '/MILOR',
+  base: '/MILORCOLOMBIA',
+  integrations: [tailwind()],
 });
