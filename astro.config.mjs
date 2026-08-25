f599515799cba879431277b8709ae8d23b4fa508
permutalibrecol <permutalibre@gmail.com>
2026-08-25 10:11:55 -0500

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://permutalibrecol.github.io',
-  base: '/MILORCOLOMBIA/',
-  integrations: [tailwind()],
-});
